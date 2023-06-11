ef279d464097540bb09b30f0e972a85562c44e10
kesanka <dnchekhin@gmail.com>
2023-06-11 16:32:46 +0300

    styles

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import './NavBar.css'
 
 const NavBar = () => {
     return (
         <nav className='nav'>
-        <div>
+        <div className = 'item'>
           <a>Profile</a>
         </div>
-        <div>
+        <div className = 'item'>
           <a>Messages</a>
         </div>
-        <div>
+        <div className = 'item'>
           <a>News</a>
         </div>
-        <div>
+        <div className = 'item'>
           <a>Music</a>
         </div>
-        <div>
+        <div className = 'item'>
           <a>Settings</a>
         </div>
       </nav>
