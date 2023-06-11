0da47e6cd26c9d5cf5c4254f33d8072b845ab4b4
kesanka <dnchekhin@gmail.com>
2023-06-11 15:40:54 +0300

    Started

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.citypng.com/public/uploads/preview/-21602609759jz7anpnlii.png' />
+        </header>
+    );
+
+};
+export default Header
---
+++ my-app/src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img src='https://funkylife.in/wp-content/uploads/2021/08/good-evening-image-from-funkylife-27.jpg' />
+        </div>
+        <div>
+          <img src='https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3' />
+        </div>
+        <div>My posts
+          <div>
+            NewPost
+          </div>
+          <div>
+            post1
+          </div>
+          <div>
+            post2
+          </div>
+        </div>
+
+      </div>
+    );
+
+};
+export default Profile
